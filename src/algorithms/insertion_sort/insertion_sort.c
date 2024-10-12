b2657328da78996c62960475bcfe033491ecb12c
gddeazevedo <gabrielcapuerj@gmail.com>
2024-10-12 18:08:53 -0300

    implementing insertion sort

--- src/algorithms/insertion_sort/insertion_sort.c
+++ src/algorithms/insertion_sort/insertion_sort.c
@@ -1,5 +1,27 @@
 #include "./insertion_sort.h"
 
-void insertion_sort(int array[], int n) {
+void insertion_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void insertion_sort_example() {
+    int arr[] = {5, 2, 4, 6, 1, 3, 49, -9, 1, 3};
+    int n = sizeof(arr) / sizeof(int);
+    insertion_sort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
 }
--- src/algorithms/insertion_sort/insertion_sort.h
+++ src/algorithms/insertion_sort/insertion_sort.h
@@ -1,7 +1,10 @@
 #ifndef __insertion_sort_h
 #define __insertion_sort_h
 
+#include <stdio.h>
+#include <stdlib.h>
 
 void insertion_sort(int arr[], int n);
+void insertion_sort_example();
 
 #endif // __insertion_sort_h
